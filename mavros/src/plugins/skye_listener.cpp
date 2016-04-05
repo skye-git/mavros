00d96d75b49ac4f9c40e6a589339b813b96c339d
Femer <femer90@hotmail.it>
2016-04-05 16:50:10 +0200

    testing integration between skye_ros and skye's firmware

--- mavros/src/plugins/skye_listener.cpp
+++ mavros/src/plugins/skye_listener.cpp
@@ -6,14 +6,13 @@
  * @addtogroup plugin
  * @{
  */
-
 #include <cmath>
 
 #include <mavros/mavros_plugin.h>
 #include <pluginlib/class_list_macros.h>
 
 #include <eigen_conversions/eigen_msg.h>
-#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Vector3Stamped.h>
 
 #include "skye_ros/ApplyWrenchCogBf.h"
 	
@@ -34,10 +33,13 @@ public:
 	{
 		uas = &uas_;
 
-		torque_pub = skye_listner_nh.advertise<geometry_msgs::Vector3>("/skye/attitude_ctrl_output", 10);
+		torque_pub = skye_listner_nh.advertise<geometry_msgs::Vector3Stamped>("/skye_px4/attitude_ctrl_output", 10);
 
     client_skye_ros_apply_wrench = skye_listner_nh.serviceClient<skye_ros::ApplyWrenchCogBf>("/skye_ros/apply_wrench_cog_bf");
-	}
+	
+    //frame_id = "body_frame";//not sure if required
+    seq_id = 0;
+  }
 
 	const message_map get_rx_handlers() {
 		return {
@@ -49,7 +51,8 @@ public:
 private:
 	ros::NodeHandle skye_listner_nh;
 	UAS *uas;
-	std::string frame_id;
+	//std::string frame_id;
+  int seq_id;
 
 	ros::Publisher torque_pub;	/*< attitide controller output torque in to be applied in the CoG. */
 	ros::Publisher allocation_output_pub;	/*< allocator output thrust and angle for every AU. */
@@ -60,21 +63,29 @@ private:
 	void handle_att_ctrl_out(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) 
 	{
 
-		ROS_INFO("*********************** att_ctrl_out!!!! ***************************");
-
     mavlink_attitude_ctrl_output_t attiude_ctrl_output;
     mavlink_msg_attitude_ctrl_output_decode(msg, &attiude_ctrl_output);
 
-    auto vector3_msg = boost::make_shared<geometry_msgs::Vector3>();
+    auto vector3_msg = boost::make_shared<geometry_msgs::Vector3Stamped>();
     
     // fill
-		vector3_msg->x	=	attiude_ctrl_output.M_x;
-		vector3_msg->y	=	attiude_ctrl_output.M_y;
-		vector3_msg->z	=	attiude_ctrl_output.M_z;
+    vector3_msg->header.seq = seq_id++;
+    vector3_msg->header.stamp = ros::Time::now();
+    vector3_msg->header.frame_id = "0";
+		vector3_msg->vector.x	=	attiude_ctrl_output.M_x;
+		vector3_msg->vector.y	=	attiude_ctrl_output.M_y;
+		vector3_msg->vector.z	=	attiude_ctrl_output.M_z;
 
 		// publish
 		torque_pub.publish(vector3_msg);
 
+    //Debug
+    ROS_INFO("***** seq[%d] att_ctrl_out: %f,\t%f,\t%f\t",  seq_id,
+                                                            vector3_msg->vector.x,
+                                                            vector3_msg->vector.y,
+                                                            vector3_msg->vector.z);
+    //End Debug 
+    
     // apply the torque to Gazebo using skye_ros interface
     skye_ros::ApplyWrenchCogBf  srv;
 
--- mavros/src/plugins/skye_talker.cpp
+++ mavros/src/plugins/skye_talker.cpp
@@ -33,9 +33,9 @@ public:
 	{
 		uas = &uas_;
 
-		skye_ros_imu_bf_sub = skye_talker_nh.subscribe("/skye_ros/sensor_msgs/imu_bf", 
+		skye_ros_imu_sk_sub = skye_talker_nh.subscribe("/skye_ros/sensor_msgs/imu_sk", 
 																										10, 
-																										&SkyeTalkerPlugin::imu_bf_callback, this);
+																										&SkyeTalkerPlugin::imu_sk_callback, this);
 
 	}
 
@@ -47,41 +47,51 @@ private:
 	ros::NodeHandle skye_talker_nh;
 	UAS *uas;
 
-	ros::Subscriber skye_ros_imu_bf_sub;
+	ros::Subscriber skye_ros_imu_sk_sub;
 
 	/* -*- message handlers -*- */
-	void imu_bf_callback(const sensor_msgs::ImuConstPtr &imu_bf_p) {
+	void imu_sk_callback(const sensor_msgs::ImuConstPtr &imu_sk_p) {
 
 		mavlink_message_t msg;
 		Eigen::Quaterniond q_imu; 
 		tf::Quaternion tf_quat;
 		float roll, pitch, yaw;
+		float rollspeed, pitchspeed, yawspeed;
 		float q[4];
 
 		/* Convert data to fullfill a mavlink message. */
-	  tf::quaternionMsgToTF(imu_bf_p->orientation, tf_quat);
-	  tf::quaternionTFToEigen(tf_quat, q_imu);
+		q_imu.w() = imu_sk_p->orientation.w;
+	  q_imu.x() = imu_sk_p->orientation.x;
+	  q_imu.y() = imu_sk_p->orientation.y;
+	  q_imu.z() = imu_sk_p->orientation.z;
+
 	  Eigen::Vector3d euler_angles = q_imu.matrix().eulerAngles(2, 1, 0); // Tait-Bryan, NED
 	  roll = static_cast<float>(euler_angles[2]);
 	  pitch = static_cast<float>(euler_angles[1]);
 	  yaw = static_cast<float>(euler_angles[0]);
+
+	  rollspeed = static_cast<float>(imu_sk_p->angular_velocity.x);
+	  pitchspeed = static_cast<float>(imu_sk_p->angular_velocity.y);
+	  yawspeed = static_cast<float>(imu_sk_p->angular_velocity.z);
 
 	  q[0] = static_cast<float>(q_imu.w());
 	  q[1] = static_cast<float>(q_imu.x());
 	  q[2] = static_cast<float>(q_imu.y());
 	  q[3] = static_cast<float>(q_imu.z());
 
-	  int i = 0;
-		/* Send the skye_imu_attitude_hil message to Skye. */
-		/*mavlink_msg_skye_imu_attitude_hil_pack_chan(UAS_PACK_CHAN(uas), &msg, 
-																								roll,
-																								pitch,
-																								yaw,
-																								imu_bfd_p->angular_velocity.x,
-																								imu_bf_p->angular_velocity.y,
-																								imu_bf_p->angular_velocity.z,
-																								q);
-		UAS_FCU(uas)->send_message(&msg);*/
+	  uint64_t timestamp = static_cast<uint64_t>(ros::Time::now().toNSec() / 1000.0); // in uSec
+
+		/* Send the skye_attitude_hil message to Skye. */
+		mavlink_msg_skye_attitude_hil_pack_chan(UAS_PACK_CHAN(uas), &msg, 
+																						timestamp,
+																						roll,
+																						pitch,
+																						yaw,
+																						rollspeed,
+																						pitchspeed,
+																						yawspeed,
+																						q);
+		UAS_FCU(uas)->send_message(&msg);
 
 	}
 };
