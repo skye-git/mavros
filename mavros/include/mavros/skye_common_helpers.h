77a1acd2c9a3cdb583e4479f32714ceca58bba10
Femer <femer90@hotmail.it>
2016-07-31 11:40:21 +0200

    added skye_attitude_hil plugin

---
+++ mavros/include/mavros/skye_common_helpers.h
@@ -0,0 +1,70 @@
+/****************************************************************************
+*
+*   Copyright (C) 2016 Aerotainment Labs. All rights reserved.
+*
+*   Redistribution and use in source and binary forms, with or without
+*   modification, is not permitted unless explicitely stated by the owner
+*   of the copyright.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+****************************************************************************/
+
+/**
+ * @file skye_common_helpers.h
+ * Common shared functions among Skye's plugins
+ *
+ * @author Marco Tranzatto <marco@skye.aero>
+ */
+
+
+#ifndef SKYE_COMMON_HELPERS_H
+#define SKYE_COMMON_HELPERS_H
+
+#include <mavros/mavros_uas.h>
+
+/**
+ * Set a integer parameter on the FMU.
+ */
+void set_parameter(mavros::UAS *uas, std::string param_name, int param_value);
+
+/**
+ * Set a float parameter on the FMU.
+ */
+void set_parameter(mavros::UAS *uas, std::string param_name, float param_value);
+
+/**
+ * Check if a topic can be published, based on the requested frequency.
+ *
+ * Updates automatically last_time.
+ */
+template<typename ClockType>
+bool publish_now(ClockType &last_time, const double &publishing_frequency) {
+
+  ClockType t_now = ClockType::now();
+  double time_elapsed = t_now.toSec() - last_time.toSec();
+
+  if (time_elapsed >= (1.0 / publishing_frequency)) {
+    // The topic can be sent
+    last_time = t_now;
+    return true;
+  }
+
+  // Not enough time has elapsed
+  return false;
+}
+
+
+
+#endif // SKYE_COMMON_HELPERS_H
